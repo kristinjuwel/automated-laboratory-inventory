5e6b54feb6656027d158c3fc530d5164f989789c
Jewel Malimban <krisdiajewls@gmail.com>
2024-11-03 13:56:48 +0800

    feat: add base laboratory dashboard

---
+++ src/app/(main)/lab/[labSlug]/page.tsx
@@ -0,0 +1,202 @@
+"use client";
+import Biological from "@/components/molecules/biological";
+import CalibrationLogs from "@/components/molecules/calibration-logs";
+import Chemical from "@/components/molecules/chemical";
+import Disposition from "@/components/molecules/disposition";
+import GeneralSupplies from "@/components/molecules/general";
+import PurchaseOrder from "@/components/molecules/purchase-order";
+import Reagent from "@/components/molecules/reagent";
+import ReagentDispense from "@/components/molecules/reagent-dispense";
+import StockLevel from "@/components/molecules/stock-level";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { cn } from "@/lib/utils";
+import {
+  Package,
+  Box,
+  ShoppingBag,
+  Sliders,
+  Thermometer,
+  LogOut,
+  FlaskConical,
+  Syringe,
+  Dna,
+  Microscope,
+} from "lucide-react";
+import { useState } from "react";
+import { usePathname } from "next/navigation"; // Import usePathname to get the current path
+
+const DashboardPage = () => {
+  const pathname = usePathname();
+  const labSlug = pathname?.split("/")[2];
+
+  const [activeInventoryTab, setActiveInventoryTab] = useState("biological");
+  const getIconByLab = (labSlug: string) => {
+    switch (labSlug) {
+      case "pathology":
+        return (
+          <Microscope
+            size={32}
+            strokeWidth={3}
+            className="text-teal-700 mr-3"
+          />
+        );
+      case "immunology":
+        return (
+          <Syringe size={32} strokeWidth={3} className="text-teal-700 mr-3" />
+        );
+      case "microbiology":
+        return <Dna size={32} strokeWidth={3} className="text-teal-700 mr-3" />;
+      default:
+        return (
+          <FlaskConical
+            size={32}
+            strokeWidth={3}
+            className="text-teal-700 mr-3"
+          />
+        );
+    }
+  };
+  return (
+    <div className="h-full w-full flex">
+      <div className="flex-grow">
+        <div className="flex items-center justify-center mb-8 pt-8">
+          {getIconByLab(labSlug)}
+          <h1 className="text-4xl font-bold text-teal-700 capitalize">
+            {cn(labSlug ? `${labSlug} Laboratory` : "Loading...")}
+          </h1>
+        </div>
+
+        <Tabs
+          defaultValue="inventory"
+          className="rounded-xl bg-white m-8 border-2 border-teal-100"
+        >
+          <TabsList className="flex justify-around bg-teal-50 text-black rounded-b-none py-6">
+            <TabsTrigger
+              value="inventory"
+              className="flex items-center space-x-2"
+            >
+              <Package size={20} />
+              <span className="font-medium">Inventory</span>
+            </TabsTrigger>
+            <TabsTrigger
+              value="purchaseOrder"
+              className="flex items-center space-x-2"
+            >
+              <ShoppingBag size={20} />
+              <span className="font-medium">Purchase Order</span>
+            </TabsTrigger>
+            <TabsTrigger
+              value="stockLevel"
+              className="flex items-center space-x-2"
+            >
+              <Box size={20} />
+              <span className="font-medium">Stock Level</span>
+            </TabsTrigger>
+            <TabsTrigger
+              value="disposition"
+              className="flex items-center space-x-2"
+            >
+              <LogOut size={20} />
+              <span className="font-medium">Disposition</span>
+            </TabsTrigger>
+            <TabsTrigger
+              value="calibrationLogs"
+              className="flex items-center space-x-2"
+            >
+              <Sliders size={20} />
+              <span className="font-medium">Calibration Logs</span>
+            </TabsTrigger>
+            <TabsTrigger
+              value="reagentsDispense"
+              className="flex items-center space-x-2"
+            >
+              <Thermometer size={20} />
+              <span className="font-medium">Reagents Dispense</span>
+            </TabsTrigger>
+          </TabsList>
+
+          {/* Content for each tab */}
+          <TabsContent value="inventory">
+            <div className="flex gap-2">
+              <div className="w-1/12 border-teal-100 border-r-2 px-4">
+                <ul className="space-y-2 py-4">
+                  <li
+                    className={cn(
+                      `cursor-pointer p-2 rounded-md ${
+                        activeInventoryTab === "biological"
+                          ? "bg-teal-100 text-black"
+                          : "hover:bg-teal-50"
+                      }`
+                    )}
+                    onClick={() => setActiveInventoryTab("biological")}
+                  >
+                    Biological
+                  </li>
+                  <li
+                    className={cn(
+                      `cursor-pointer p-2 rounded-md ${
+                        activeInventoryTab === "chemical"
+                          ? "bg-teal-100 text-black"
+                          : "hover:bg-teal-50"
+                      }`
+                    )}
+                    onClick={() => setActiveInventoryTab("chemical")}
+                  >
+                    Chemical
+                  </li>
+                  <li
+                    className={cn(
+                      `cursor-pointer p-2 rounded-md ${
+                        activeInventoryTab === "general"
+                          ? "bg-teal-100 text-black"
+                          : "hover:bg-teal-50"
+                      }`
+                    )}
+                    onClick={() => setActiveInventoryTab("general")}
+                  >
+                    General
+                  </li>
+                  <li
+                    className={cn(
+                      `cursor-pointer p-2 rounded-md ${
+                        activeInventoryTab === "reagent"
+                          ? "bg-teal-100 text-black"
+                          : "hover:bg-teal-50"
+                      }`
+                    )}
+                    onClick={() => setActiveInventoryTab("reagent")}
+                  >
+                    Reagent
+                  </li>
+                </ul>
+              </div>
+              <div className="w-11/12">
+                {activeInventoryTab === "biological" && <Biological />}
+                {activeInventoryTab === "chemical" && <Chemical />}
+                {activeInventoryTab === "general" && <GeneralSupplies />}
+                {activeInventoryTab === "reagent" && <Reagent />}
+              </div>
+            </div>
+          </TabsContent>
+          <TabsContent value="purchaseOrder" className="h-full">
+            <PurchaseOrder />
+          </TabsContent>
+          <TabsContent value="stockLevel">
+            <StockLevel />
+          </TabsContent>
+          <TabsContent value="disposition">
+            <Disposition />
+          </TabsContent>
+          <TabsContent value="calibrationLogs">
+            <CalibrationLogs />
+          </TabsContent>
+          <TabsContent value="reagentsDispense">
+            <ReagentDispense />
+          </TabsContent>
+        </Tabs>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardPage;
---
+++ src/components/molecules/general.tsx
@@ -0,0 +1,308 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Toaster } from "sonner";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Edit, Search, TriangleAlert, FilePlus, Printer } from "lucide-react";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { cn } from "@/lib/utils";
+
+interface User {
+  id: string;
+  lastName: string;
+  firstName: string;
+  middleName: string;
+  designation: string;
+  laboratory: string;
+  email: string;
+  username: string;
+  status: string;
+}
+
+const GeneralSupplies = () => {
+  const [users, setUsers] = useState<User[]>([]);
+  const [filteredUsers, setFilteredUsers] = useState<User[]>([]);
+  const [search, setSearch] = useState("");
+  const [isEditDialogOpen, setIsEditDialogOpen] = useState(false);
+  const [isDeleteDialogOpen, setIsDeleteDialogOpen] = useState(false);
+  const [isCreateDialogOpen, setIsCreateDialogOpen] = useState(false);
+  const [selectedUser, setSelectedUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const fetchData = [
+      {
+        id: "1",
+        lastName: "Doe",
+        firstName: "John",
+        middleName: "A.",
+        designation: "Researcher",
+        laboratory: "Pathology",
+        email: "john.doe@example.com",
+        username: "jdoe",
+        status: "active",
+      },
+      {
+        id: "2",
+        lastName: "Smith",
+        firstName: "Jane",
+        middleName: "B.",
+        designation: "Technician",
+        laboratory: "Immunology",
+        email: "jane.smith@example.com",
+        username: "jsmith",
+        status: "active",
+      },
+      {
+        id: "3",
+        lastName: "Brown",
+        firstName: "Alex",
+        middleName: "C.",
+        designation: "Lab Manager",
+        laboratory: "Microbiology",
+        email: "alex.brown@example.com",
+        username: "abrown",
+        status: "active",
+      },
+    ];
+    setUsers(fetchData);
+    setFilteredUsers(fetchData);
+  }, []);
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const query = e.target.value;
+    setSearch(query);
+
+    setFilteredUsers(
+      users.filter((user) =>
+        `${user.firstName} ${user.lastName} ${user.middleName}`
+          .toLowerCase()
+          .includes(query.toLowerCase())
+      )
+    );
+  };
+
+  return (
+    <div className=" p-8">
+      <h1 className="text-3xl font-semibold text-teal-700 mb-4">
+        General Supplies Inventory Forms
+      </h1>
+      <div className="flex text-right justify-left items-center mb-4">
+        <div className="flex items-center">
+          <Input
+            placeholder="Search for an entry"
+            value={search}
+            onChange={handleSearch}
+            className="w-80 pr-8"
+          />
+          <span className="relative -ml-8">
+            <Search className="size-5 text-gray-500" />
+          </span>
+
+          <Button
+            className={cn(
+              `bg-teal-500 text-white w-36 justify-center rounded-lg hover:bg-teal-700 transition-colors duration-300 ease-in-out mx-6`
+            )}
+            onClick={() => {
+              setIsCreateDialogOpen(true);
+            }}
+          >
+            <FilePlus className="w-4 h-4" strokeWidth={1.5} />
+            Create Form
+          </Button>
+        </div>
+      </div>
+
+      <Toaster />
+
+      <Table className="items-center justify-center">
+        <TableHeader className="text-center justify-center">
+          <TableRow>
+            <TableHead>Id</TableHead>
+            <TableHead>Name</TableHead>
+            <TableHead>Designation</TableHead>
+            <TableHead>Laboratory</TableHead>
+            <TableHead>Status</TableHead>
+            <TableHead className="text-center">Username</TableHead>
+            <TableHead className="text-center">Email</TableHead>
+            <TableHead className="text-center">Actions</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {filteredUsers.length > 0 ? (
+            filteredUsers.map((user) => (
+              <TableRow key={user.id}>
+                <TableCell>{user.id}</TableCell>
+                <TableCell>{`${user.lastName}, ${user.firstName} ${user.middleName}`}</TableCell>
+                <TableCell>{user.designation}</TableCell>
+                <TableCell>{user.laboratory}</TableCell>
+                <TableCell>{user.status}</TableCell>
+                <TableCell className="text-center">{user.username}</TableCell>
+                <TableCell className="text-center">{user.email}</TableCell>
+                <TableCell className="text-center">
+                  <Button
+                    variant="ghost"
+                    size="sm"
+                    className="rounded-md text-cyan-600 hover:text-cyan-900 hover:bg-cyan-50"
+                    onClick={() => {
+                      setSelectedUser(user);
+                      setIsEditDialogOpen(true);
+                    }}
+                  >
+                    <Edit className="w-4 h-4 -mr-0.5" /> Edit
+                  </Button>
+                  <Button
+                    variant="ghost"
+                    size="sm"
+                    className="rounded-md text-red-600 hover:text-red-900 hover:bg-red-50"
+                    onClick={() => {
+                      setSelectedUser(user);
+                      setIsDeleteDialogOpen(true);
+                    }}
+                  >
+                    <Printer className="w-4 h-4 -mr-1" /> Print
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={8} className="text-center text-gray-500">
+                No users found.
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+
+      <Dialog open={isEditDialogOpen} onOpenChange={setIsEditDialogOpen}>
+        <DialogContent className="bg-white max-h-4/5 overflow-y-auto">
+          <DialogHeader>
+            <DialogTitle>Edit User</DialogTitle>
+          </DialogHeader>
+          <div>
+            <Input
+              value={selectedUser?.firstName}
+              placeholder="First Name"
+              className="mb-4"
+            />
+            <Input
+              value={selectedUser?.middleName}
+              placeholder="Last Name"
+              className="mb-4"
+            />
+            <Input
+              value={selectedUser?.lastName}
+              placeholder="Last Name"
+              className="mb-4"
+            />
+            <Input
+              value={selectedUser?.designation}
+              placeholder="Last Name"
+              className="mb-4"
+            />
+            <Input
+              value={selectedUser?.laboratory}
+              placeholder="Last Name"
+              className="mb-4"
+            />
+            <Input
+              value={selectedUser?.email}
+              placeholder="Last Name"
+              className="mb-4"
+            />
+            <Input
+              value={selectedUser?.username}
+              placeholder="Last Name"
+              className="mb-4"
+            />
+            <div className="relative">
+              <Button
+                className="absolute right-0 mr-4"
+                onClick={() => setIsEditDialogOpen(false)}
+              >
+                Save Changes
+              </Button>
+            </div>
+          </div>
+        </DialogContent>
+      </Dialog>
+
+      <Dialog open={isDeleteDialogOpen} onOpenChange={setIsDeleteDialogOpen}>
+        <DialogContent className="bg-white">
+          <DialogHeader>
+            <DialogTitle className="flex items-center gap-2 tracking-tight">
+              <TriangleAlert className="text-red-500 size-5 -mt-0.5" />
+              Delete User
+            </DialogTitle>
+          </DialogHeader>
+          <p className="text-left pt-2 text-sm">
+            Are you sure you want to delete this user?
+          </p>
+          <p className="text-left bg-red-300 -mt-2 relative py-2 text-sm">
+            <span className="pl-4">
+              By deleting this user, they will be removed indefinitely.
+            </span>
+            <span className="absolute left-0 top-0 h-full w-2 bg-red-600"></span>
+          </p>
+          <div className="flex justify-end gap-2 mt-2">
+            <Button
+              variant="ghost"
+              className="bg-gray-100"
+              onClick={() => setIsDeleteDialogOpen(false)}
+            >
+              Cancel
+            </Button>
+            <Button
+              variant="destructive"
+              onClick={() => setIsDeleteDialogOpen(false)}
+            >
+              Confirm
+            </Button>
+          </div>
+        </DialogContent>
+      </Dialog>
+
+      <Dialog open={isCreateDialogOpen} onOpenChange={setIsCreateDialogOpen}>
+        <DialogContent className="bg-white">
+          <DialogHeader>
+            <DialogTitle className="flex items-center gap-2 tracking-tight mb-4">
+              <FilePlus className="text-teal-500 size-5 -mt-0.5" />
+              Add User
+            </DialogTitle>
+          </DialogHeader>
+          <div className="flex justify-end gap-2">
+            <Button
+              variant="ghost"
+              className="bg-gray-100"
+              onClick={() => setIsCreateDialogOpen(false)}
+            >
+              Cancel
+            </Button>
+
+            <Button
+              className="bg-teal-500 text-white hover:bg-teal-700 transition-colors duration-300 ease-in-out"
+              onClick={() => setIsCreateDialogOpen(false)}
+            >
+              Add User
+            </Button>
+          </div>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+};
+
+export default GeneralSupplies;
