99b9b6d8dc55cbdc3648926c19559cdd903e1835
Nabi-nah <113338569+Nabi-nah@users.noreply.github.com>
2024-11-18 14:19:35 +0800

    dispo report fix

--- src/app/(main)/(user)/disposition-report/page.tsx
+++ src/app/(main)/(user)/disposition-report/page.tsx
@@ -2,6 +2,241 @@
 
 import React from "react";
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Card } from "@/components/ui/card";
+import { Toaster, toast } from "sonner";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+} from "@/components/ui/form";
+import { DatePickerWithPresets } from "@/components/ui/datepicker";
+
+interface DispositionReportValues {
+  description: string;
+  quantity: number;
+  reason: string;
+  method: string;
+  date: string;
+  dispositionedBy: string;
+  comments: string;
+}
+
+const DispositionReportForm = () => {
+    const form = useForm<DispositionReportValues>({
+      defaultValues: {
+        description: "",
+        quantity: undefined,
+        reason: "",
+        method: "",
+        date: "",
+        dispositionedBy: "",
+        comments: "",
+      },
+    });
+  
+    const handleSubmit = async (values: DispositionReportValues) => {
+      const parsedValues = {
+        ...values,
+        quantity: Number(values.quantity),
+      };
+  
+      try {
+        console.log("Submitted Values:", parsedValues);
+        toast.success("Submission successful!");
+        form.reset();
+      } catch (error) {
+        console.error("Error submitting form:", error);
+        toast.error("Submission failed. Please try again.");
+      }
+    };
+  
+    return (
+      <div className="flex w-screen h-screen justify-center items-center bg-gray-100">
+        <Card className="p-8 w-full max-w-[935px] max-h-[700px] shadow-lg">
+          <div className="flex flex-col items-center mb-4">
+            <div className="flex space-x-4 mb-4">
+              <div className="w-24 h-24 relative">
+                <Image
+                  src="/images/mrl-logo.png"
+                  alt="Logo 1"
+                  fill
+                  style={{ objectFit: "contain" }}
+                />
+              </div>
+              <div className="w-24 h-24 relative">
+                <Image
+                  src="/images/pgh-logo.png"
+                  alt="Logo 2"
+                  fill
+                  style={{ objectFit: "contain" }}
+                />
+              </div>
+            </div>
+            <h1 className="text-xl font-bold py-1">Disposition Report Form</h1>
+            <hr className="w-full border-t-1 border-gray-300 my-1" />
+          </div>
+  
+          <Toaster />
+  
+          <div className="overflow-y-auto max-h-[430px] mb-1">
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(handleSubmit)} className="mb-4">
+                <div className="grid grid-cols-3 gap-3 mb-4">
+               
+                  <FormField
+                    name="date"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Disposition Date</FormLabel>
+                        <FormControl>
+                          <DatePickerWithPresets
+                            date={field.value}
+                            setDate={(newDate) => field.onChange(newDate)}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    name="description"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Item Description</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder=" Description of Item"
+                            {...field}
+                            required
+                            className="w-full"
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                  name="quantity"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Quantity</FormLabel>
+                      <FormControl>
+                        <Input
+                          type="number"
+                          placeholder="Quantity"
+                          {...field}
+                          required
+                          className="w-full"
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                </div>
+  
+                <div className="grid grid-cols-2 gap-4 mb-4">
+                <FormField
+                    name="reason"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Reason for Disposition</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="Why it was disposed..."
+                            {...field}
+                            required
+                            className="w-full"
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+
+                <FormField
+                    name="method"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Disposition Method</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="How it was disposed?"
+                            {...field}
+                            required
+                            className="w-full"
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+         
+                </div>
+                <FormField
+                  name="dispositionedBy"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Notes</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Name"
+                          {...field}
+                          className="w-full"
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />       
+                <FormField
+                  name="comments"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Commments</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Any relevant information..."
+                          {...field}
+                          className="w-full"
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <div className="flex justify-center mt-8">
+                  <Button
+                    type="submit"
+                    onClick={form.handleSubmit(handleSubmit)}
+                    className="bg-teal-500 text-white w-full hover:bg-teal-700 transition-colors duration-300 ease-in-out"
+                  >
+                    Submit Form
+                  </Button>
+                </div>
+              </form>
+            </Form>
+          </div>
+        </Card>
+      </div>
+    );
+  };
+  
+  export default DispositionReportForm;
+
+/////////////OLD CODE/////////
+
+/*
+"use client";
+
+import React from "react";
+import Image from "next/image";
 import { Card } from "@/components/ui/card";
 import { Toaster } from "sonner";
 
@@ -104,3 +339,5 @@ const DispositionReport = () => {
 };
 
 export default DispositionReport;
+
+*/
