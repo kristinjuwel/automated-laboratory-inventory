3b9a25c5f37d00342c07dda0636d80532b4099ad
Nabi-Nah <oysuba@up.edu.ph>
2024-11-24 23:25:13 +0800

    Responsiveness Update

--- src/app/(main)/(user)/incident-form/page.tsx
+++ src/app/(main)/(user)/incident-form/page.tsx
@@ -63,11 +63,11 @@ const IncidentForm = () => {
   };
 
   return (
-    <div className="flex w-full min-h-screen justify-center items-center bg-gray-100 px-4 sm:px-6 lg:px-8">
-      <Card className="p-6 sm:p-8 md:p-10 lg:p-12 w-full max-w-[935px] sm:max-w-[700px] lg:max-w-[900px] shadow-lg">
+    <div className="flex justify-center items-center bg-gray-100 w-full h-screen">
+      <Card className="p-8 w-full max-w-[935px] max-h-[700px] shadow-lg">
         <div className="flex flex-col items-center mb-4">
           <div className="flex space-x-4 mb-4">
-            <div className="w-16 h-16 sm:w-20 sm:h-20 md:w-24 md:h-24 relative">
+            <div className="w-20 h-20 sm:w-24 sm:h-24 relative">
               <Image
                 src="/images/mrl-logo.png"
                 alt="Logo 1"
@@ -75,7 +75,7 @@ const IncidentForm = () => {
                 style={{ objectFit: "contain" }}
               />
             </div>
-            <div className="w-16 h-16 sm:w-20 sm:h-20 md:w-24 md:h-24 relative">
+            <div className="w-20 h-20 sm:w-24 sm:h-24 relative">
               <Image
                 src="/images/pgh-logo.png"
                 alt="Logo 2"
@@ -84,7 +84,7 @@ const IncidentForm = () => {
               />
             </div>
           </div>
-          <h1 className="text-lg sm:text-xl md:text-2xl font-bold py-1">
+          <h1 className="text-lg sm:text-xl font-bold py-1 text-center">
             Incident Form
           </h1>
           <hr className="w-full border-t-1 border-gray-300 my-1" />
@@ -92,14 +92,14 @@ const IncidentForm = () => {
 
         <Toaster />
 
-        <div className="overflow-y-auto max-h-[400px] sm:max-h-[500px] md:max-h-[600px] mb-4">
+        <div className="overflow-y-auto max-h-[400px] mb-1">
           <Form {...form}>
             <form
               onSubmit={form.handleSubmit(handleSubmit)}
               className="space-y-6"
             >
               {/* Date and Time */}
-              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+              <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-3 gap-6 mb-6">
                 <FormField
                   name="date"
                   render={({ field }) => (
